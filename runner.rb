0697baf5eca6782d2fa1e4328cdae6572febc7a9
Tyler Porter <tylerpporter@gmail.com>
2020-02-06 00:13:35 -0700

    Update runner file with GameSetup class

--- runner.rb
+++ runner.rb
@@ -2,8 +2,8 @@ require 'require_all'
 require_all './lib'
 
 menu = MainMenu.new
-menu 
+
 if menu.user_decision == 'p'
-  #placeholder for a Setup class
-  puts Board.new.render
+  game_setup = GameSetup.new
+  game_setup.create_ships
 end

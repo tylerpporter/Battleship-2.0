a58c1995c3808ba5ee09d1b580cc96a1dd7d52ae
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-06 14:32:08 -0700

    added valid placement method and test

--- lib/board.rb
+++ lib/board.rb
@@ -33,6 +33,23 @@ attr_reader :cells
   def valid_placement?(ship, coordinate)
       return false if ship.length != coordinate.size
       return false if coordinate.any? {|key| @cells[key].ship != nil}
+
+      rows = coordinate.map(&:chars).map { |arr| arr[0] }
+      columns = coordinate.map(&:chars).map { |arr| arr[1] }
+
+      if rows.uniq.size == 1 &&
+        columns.map(&:to_i).each_cons(2).all? {|num1, num2| num2 == num1 + 1}
+        true
+      elsif
+        columns.uniq.size == 1 &&
+        rows.map(&:ord).each_cons(2).all? {|num1, num2| num2 == num1 + 1}
+        true
+      else
+        false
+      end
+
+
   end
+
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -30,9 +30,17 @@ class BoardTest < Minitest::Test
     assert @board.valid_placement?(@ship, ["A1", "A2"])
     assert @board.valid_placement?(@ship2, ["C1", "C2", "C3"])
     assert_equal false, @board.valid_placement?(@ship, ["C1", "C2", "C3"])
-    
 
+  end
 
+  def test_ships_cannot_be_placed_diagonally
+    assert_equal false, @board.valid_placement?(@ship2,
+     ["A2", "B3", "C4"])
+  end
+  def test_ships_can_only_be_placed_in_consecutive_coordinates
+    assert @board.valid_placement?(@ship2, ["B1", "C1",
+      "D1"])
+    assert_equal false, @board.valid_placement?(@ship2, ["A1", "A3", "A4"])
   end
 
 end

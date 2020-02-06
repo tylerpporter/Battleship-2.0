baf8b4cdc7bb2614cac0d5181fea90e9700a010b
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-05 17:19:23 -0700

    added instance variable/hash as well as tests

--- test/board_test.rb
+++ test/board_test.rb
@@ -5,9 +5,21 @@ class BoardTest < Minitest::Test
   def setup
     @board = Board.new
   end
-  
-  def test_it_exists
+
+  def test_it_exists_with_cells
     assert_instance_of Board, @board
+    assert_equal 16, @board.cells.size
+    assert_equal Hash, @board.cells.class
+  end
+
+  def test_each_cell_is_a_cell_object
+    assert_equal Cell, @board.cells["A1"].class
+
+  end
+
+  def test_it_can_validate_coordinates
+  assert_equal true, @board.valid_coordinate?("A1")
+  assert_equal false, @board.valid_coordinate?("Z1")
 
   end
 end
